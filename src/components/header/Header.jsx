496896961b14c9ae83685743893652f464e9605e
Abhishek De <deabhishek06@gmail.com>
2024-05-15 20:16:55 +0530

    Added search bar in Header.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,37 @@
-import "./Header.scss"
-import { Link } from "react-router-dom"
-import user from "../../assets/user.png"
+import "./Header.scss";
+import { Link } from "react-router-dom";
+import user from "../../assets/user.png";
+import { useState } from "react";
 
 const Header = () => {
+  const [term, setTerm] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(term);
+  };
   return (
     <div className="header">
-    <Link to="/">
-      <div className="logo">Movie App</div>
-    </Link>
-    <div className="user-image">
-      <img src={user} alt="user" />
+      <div className="logo">
+        <Link to="/">Movie App</Link>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={term}
+            placeholder="Search Movies or Shows"
+            onChange={(e) => setTerm(e.target.value)}
+          />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
